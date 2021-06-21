17865838328f4db865905711f8c3fccb375374ac
Kenia Rodriguez <keniaaqrodriguez@gmail.com>
2021-06-21 17:10:09 -0400

    update api url

--- src/Util/apiURL.js
+++ src/Util/apiURL.js
@@ -1,16 +1,9 @@
-export const apiURL = () => {
-    return window.location.hostname === "localhost"
-      ? "http://localhost:3001"
-      : "https://budgeting-app-backend-express.herokuapp.com/";
+  export const apiURL = () => {
+    if (
+      process.env.NODE_ENV === "development" ||
+      process.env.NODE_ENV === "test"
+    ) {
+      return "http://localhost:3001";
+    }
+    return "https://budgeting-app-backend-express.herokuapp.com";
   };
-
-//   export const apiURL = () => {
-//     // development // production // test
-//     if (
-//       process.env.NODE_ENV === "development" ||
-//       process.env.NODE_ENV === "test"
-//     ) {
-//       return "http://localhost:3001";
-//     }
-//     return null;
-//   };
